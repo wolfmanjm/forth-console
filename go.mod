06337fa141aba9e211206082a3c224f2822a1e78
Jim Morris <morris@wolfman.com>
2026-02-16 21:25:25 +0000

    initial

---
+++ go.mod
@@ -0,0 +1,18 @@
+module forthcon
+
+go 1.25.5
+
+require (
+	github.com/chzyer/readline v1.5.1
+	golang.design/x/clipboard v0.7.1
+)
+
+require github.com/creack/goselect v0.1.2 // indirect
+
+require (
+	go.bug.st/serial v1.6.4
+	golang.org/x/exp/shiny v0.0.0-20250606033433-dcc06ee1d476 // indirect
+	golang.org/x/image v0.28.0 // indirect
+	golang.org/x/mobile v0.0.0-20250606033058-a2a15c67f36f // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,186 @@
+package main
+
+// forth console somewhat like e4thcom but not quite as heavy
+// Adding ability to stream code using the dl word
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/chzyer/readline"
+	"go.bug.st/serial"
+	"golang.design/x/clipboard"
+)
+
+var completer = readline.NewPrefixCompleter()
+func filterInput(r rune) (rune, bool) {
+	switch r {
+	// block CtrlZ feature
+	case readline.CharCtrlZ:
+		return r, false
+	}
+	return r, true
+}
+
+// stores the last line recieved
+// useful for checking the ok.
+var lastLine string
+var cond *sync.Cond = sync.NewCond(&sync.Mutex{})
+
+func main() {
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:      "\033[31m»\033[0m ",
+		HistoryFile: "./.history",
+		// AutoComplete:    completer,
+		InterruptPrompt:     "^C",
+		EOFPrompt:           "exit",
+		UniqueEditLine:      true,
+		HistorySearchFold:   true,
+		FuncFilterInputRune: filterInput,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
+
+	// get flags
+	dev := flag.String("d", "/dev/ttyUSB0", "Set serial port")
+	baud := flag.Int("b", 3000000, "Set serial baudrate")
+
+	flag.Parse()
+
+	// open serial port
+	mode := &serial.Mode{
+		BaudRate: *baud,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	s, err := serial.Open(*dev, mode)
+	if err != nil {
+		panic("unable to open serial port: " + *dev + ": " + err.Error())
+	}
+	defer s.Close()
+
+	// serial reader
+	ch := make(chan string, 100)
+	defer close(ch)
+
+	go readLoop(s, ch)
+
+	// display any lines we get
+	go func() {
+		for s := range ch {
+			fmt.Fprint(l.Stdout(), s)
+			cond.L.Lock()
+			lastLine = strings.TrimSuffix(s, "\n")
+			cond.L.Unlock()
+			cond.Signal()
+		}
+	}()
+
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+
+		if strings.HasPrefix(line, "\\") {
+			handleCommand(l, s, line[1:])
+		} else {
+			s.Write([]byte(line + "\n"))
+		}
+	}
+}
+
+func handleCommand(l *readline.Instance, s serial.Port, line string) {
+	switch {
+	case strings.HasPrefix(line, "q"):
+		os.Exit(1)
+	case strings.HasPrefix(line, "d"):
+		fmt.Fprintln(l.Stderr(), "Download file: " + line[1:])
+
+	case strings.HasPrefix(line, "p"):
+		fmt.Fprintln(l.Stderr(), "Paste with handshaking")
+		err := clipboard.Init()
+		if err == nil {
+			// Read from clipboard
+			str := string(clipboard.Read(clipboard.FmtText))
+			sendWithOk(l, s, str)
+		} else {
+			fmt.Fprintln(l.Stderr(), "Clipboard error: ", err.Error())
+		}
+
+	case strings.HasPrefix(line, "br"):
+		fmt.Fprintln(l.Stderr(), "Send Break")
+	default:
+		fmt.Fprintln(l.Stderr(), "Unknown command: " + line)
+		return
+	}
+}
+
+// send each line of the string and wait for ok. or other error
+func sendWithOk(l *readline.Instance, s serial.Port, str string) {
+	li := strings.SplitSeq(str, "\n")
+	for ln := range li {
+		s.Write([]byte(ln + "\n"))
+		cond.L.Lock()
+		// waits until we get a reply
+		cond.Wait()
+		if !strings.HasSuffix(lastLine, "ok.") {
+			fmt.Fprintln(l.Stderr(), "Got error: ", lastLine)
+			cond.L.Unlock()
+			break
+		}
+		cond.L.Unlock()
+	}
+}
+
+// this reads from the serial line and sends whole lines to the channel
+// it buffers up partial lines
+func readLoop(s serial.Port, ch chan string) {
+	buf := make([]byte, 1024)
+	var rdBuf bytes.Buffer
+
+	for {
+		// Reads up to length of buf (which is 1024 bytes)
+		n, err := s.Read(buf)
+		if err != nil {
+			fmt.Println("readLoop exiting with error: " + err.Error())
+			return
+		}
+
+		// if we have a partial line left over from last time then append new data to it
+		if rdBuf.Len() > 0 {
+			rdBuf.Write(buf[:n])
+			// this reads the last fragment prepended to the new data
+			n, _ = rdBuf.Read(buf)
+			if n >= len(buf) {
+				fmt.Println("line too long, discarded")
+				rdBuf.Reset()
+				continue
+			}
+		}
+
+		// process each line
+	 	for l := range bytes.Lines(buf[:n]) {
+			if bytes.HasSuffix(l, []byte("\n")) {
+				ch <- string(l)
+			} else {
+				rdBuf.Write(l)
+			}
+		}
+	}
+}
